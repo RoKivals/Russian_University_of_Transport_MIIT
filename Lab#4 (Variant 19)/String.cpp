def6ea0acefb2128a60dcfcf43442e9e9fac7287
slava <slava111103@gmail.com>
2022-06-11 21:59:51 +0300

    Была решена проблема в реализации сложения. Теперь любая сумма двух чаров попадает в промежуток человечочитаемых символов ASCII.

--- Lab#4 (Variant 19)/String.cpp
+++ Lab#4 (Variant 19)/String.cpp
@@ -13,13 +13,13 @@ String::String() {
 String::String(const unsigned char *arr, unsigned int size) {
   arr_ = new unsigned char[max_size]{0};
   if (arr == nullptr || size + 1 > max_size || size < 0) {
-	size_ = 0;
+    size_ = 0;
   } else {
-	size_ = size + 1;
-	for (unsigned int x = 1; x < size; x++) {
-	  this->arr_[x] = arr[x - 1];
-	}
-	this->arr_[0] = char(size);
+    size_ = size + 1;
+    for (unsigned int x = 1; x < size; x++) {
+      this->arr_[x] = arr[x - 1];
+    }
+    this->arr_[0] = char(size);
   }
 }
 
@@ -33,32 +33,32 @@ String &String::operator+(String &right) {
   int itog_size = std::max(this->size_, right.size_);
   // Если один из массивов больше, просто перенесём хвост путём присваивания
   if (right.size_ > size_) {
-	for (int x = itog_size - 1; x >= size_; x--) {
-	  res[x] = right.arr_[x];
-	}
-	for (int x = 1; x < itog_size; x++) {
-	  if (arr_[x] + right.arr_[x] >= 126) {
-		res[x] = (arr_[x] + right.arr_[x]) % 127 + 32;
-	  } else
-		res[x] = (arr_[x] + right.arr_[x]);
-	}
+    for (int x = itog_size - 1; x >= size_; x--) {
+      res[x] = right.arr_[x];
+    }
+    for (int x = 1; x < itog_size; x++) {
+      if (arr_[x] + right.arr_[x] >= 126) {
+        res[x] = 31 + (arr_[x] + right.arr_[x]) % (127 - 31 + 1);
+      } else
+        res[x] = (arr_[x] + right.arr_[x]);
+    }
   } else if (size_ > right.size_) {
-	for (int x = itog_size - 1; x >= right.size_; x--) {
-	  res[x] = right.arr_[x];
-	}
-	for (int x = 1; x < right.size_; x++) {
-	  if (arr_[x] + right.arr_[x] >= 126) {
-		res[x] = (arr_[x] + right.arr_[x]) % 127 + 32;
-	  } else
-		res[x] = (arr_[x] + right.arr_[x]);
-	}
+    for (int x = itog_size - 1; x >= right.size_; x--) {
+      res[x] = right.arr_[x];
+    }
+    for (int x = 1; x < right.size_; x++) {
+      if (arr_[x] + right.arr_[x] >= 126) {
+        res[x] = 31 + (arr_[x] + right.arr_[x]) % (127 - 31 + 1);
+      } else
+        res[x] = (arr_[x] + right.arr_[x]);
+    }
   } else {
-	for (int x = 1; x < itog_size; x++) {
-	  if (arr_[x] + right.arr_[x] >= 126) {
-		res[x] = (arr_[x] + right.arr_[x]) % 127 + 32;
-	  } else
-		res[x] = (arr_[x] + right.arr_[x]);
-	}
+    for (int x = 1; x < itog_size; x++) {
+      if (arr_[x] + right.arr_[x] >= 126) {
+        res[x] = 31 + (arr_[x] + right.arr_[x]) % (127 - 31 + 1);
+      } else
+        res[x] = (arr_[x] + right.arr_[x]);
+    }
   }
   auto *bs = new String(res, itog_size);
   return *bs;
@@ -66,9 +66,9 @@ String &String::operator+(String &right) {
 
 unsigned char &String::operator[](int index) {
   if (index >= 0 && index < size_) {
-	return arr_[index + 1];
+    return arr_[index + 1];
   } else {
-	std::cout << "ERROR!";
+    std::cout << "ERROR!";
   }
 }
 
