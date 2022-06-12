11fe0991400e123e095558846e0f973b60f488c1
slava <slava111103@gmail.com>
2022-06-12 20:19:17 +0300

    Фиксы. Чистка кода. 1. Исправил все ошибки в сложении Array.cpp, работает как надо во всех случаях размерности массивов и переполнения разряда. 2. Переделана также сама конструкция класса Array, теперь массив чаров внутри инициализируется символом '0' по умолчанию. 3. Улучшена читаемость кода + комментарии к нему

--- Lab#4/main.cpp
+++ Lab#4/main.cpp
@@ -14,36 +14,38 @@ void menu() {
   try {
 	if (choice == 1) {
 	  auto *ar1 = new unsigned char[max_size]{0};
-	  ar1 = (unsigned char *)"1231240";
+	  ar1 = (unsigned char *) "2231280";
 	  auto *ar2 = new unsigned char[max_size]{0};
-	  ar2 = (unsigned char *)"111234";
+	  ar2 = (unsigned char *) "911234";
 	  std::cout << "Сумма двух массивов:\n";
-	  Array arr1(ar1, 7), arr2(ar2, 6), ar33;
+	  Array arr1(ar1, 7), arr2(ar2, 6), ar_res;
+	  ar_res = arr1 + arr2;
 	  std::cout << " ";
 	  for (int i = 0; i < arr1.get_size(); i++) {
-		std::cout << ar1[i];
+		std::cout << arr1[i];
 	  }
 	  std::cout << endl << "+\n ";
 	  for (int i = 0; i < arr2.get_size(); i++) {
-		std::cout << ar2[i];
+		std::cout << arr2[i];
 	  }
 	  std::cout << endl << " ";
 	  for (int i = 1; i < max_size; i++) {
 		std::cout << "-";
 	  }
 	  std::cout << endl << " ";
-	  ar33 = arr1 + arr2;
-	  for (int i = 0; i < ar33.get_size(); i++) {
-		cout << ar33[i];
+
+	  for (int i = 0; i < ar_res.get_size(); i++) {
+		cout << ar_res[i];
 	  }
 	  cout << endl;
 	} else if (choice == 2) {
 	  auto *ar1 = new unsigned char[max_size]{0};
-	  ar1 = (unsigned char *)"123400";
+	  ar1 = (unsigned char *) "123400";
 	  auto *ar2 = new unsigned char[max_size]{0};
-	  ar2 = (unsigned char *)"245600";
+	  ar2 = (unsigned char *) "245600";
 	  cout << "Сумма двух денежных сумм:\n\n";
-	  Money mon1(ar1, 6), mon2(ar2, 6), mon3;
+	  Money mon1(ar1, 6), mon2(ar2, 6), mon_res;
+	  mon_res = mon1 + mon2;
 	  cout << " ";
 	  for (int i = 0; i < mon1.get_size(); i++) {
 		cout << ar1[i];
@@ -59,17 +61,16 @@ void menu() {
 		cout << "-";
 	  }
 	  cout << endl << " ";
-	  mon3 = mon1 + mon2;
-	  for (int i = 0; i < mon3.get_size(); i++) {
-		cout << mon3[i];
+	  for (int i = 0; i < mon_res.get_size(); i++) {
+		cout << mon_res[i];
 		if (i == mon1.get_size() - 3) cout << " ";
 	  }
 	  cout << endl;
 	} else if (choice == 3) {
 	  auto *ar1 = new unsigned char[max_size]{0};
-	  ar1 = (unsigned char *)"1010101";
+	  ar1 = (unsigned char *) "1010101";
 	  auto *ar2 = new unsigned char[max_size]{0};
-	  ar2 = (unsigned char *)"1000100";
+	  ar2 = (unsigned char *) "1000100";
 	  cout << "Сумма двух битовых строк:\n\n";
 	  BitString bs1(ar1, 7), bs2(ar2, 7), bs3;
 	  cout << " ";
@@ -94,15 +95,14 @@ void menu() {
 	  throw string("Ошибка! Повторите ввод!");
 	}
   }
-  catch (
-	  string &m
-  ) {
-	cout << m << endl <<endl;
+  catch (string &m) {
+	cout << m << endl << endl;
 	menu();
   }
+  catch (logic_error& le) {
+	cout << le.what() << endl;
+  }
 }
-
-
 
 int main() {
   SetConsoleCP(1251);
